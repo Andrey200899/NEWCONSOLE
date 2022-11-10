876a082f36a238e47ca15b793d624747afd47d7d
unknown <kvkvachev@yandex.ru>
2022-11-10 19:57:41 +0300

    Example005

---
+++ Example_001 NEWCONSOLE/Example003 SUM/Example005/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите имя пользователя :");
+string username = Console.ReadLine();
+
+if(username == "Ангелина")
+
+{
+    Console.WriteLine ("Люблю Ангелинку бараненка!");
+}
+else 
+
+ {
+    Console.Write("Привет,");
+    Console.WriteLine ("username");
+ }
+
